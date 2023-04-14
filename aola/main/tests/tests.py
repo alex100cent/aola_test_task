5bb25f9952ed07efd29095f68c961cb45ebba45b
Стаценко Александр <astatsenko@datana.ru>
2023-04-14 11:59:15 +0600

    add factoryboy

--- aola/main/tests/tests.py
+++ aola/main/tests/tests.py
@@ -1,21 +1,14 @@
 import pytest
-from pytest_factoryboy import register
 
-from .factories import UserFactory, AdFactory, AchievementFactory, NoteFactory, UsersPostsFactory
 from ..constants import FilterKeyWords
 from ..models import Achievement, Ad, Note
 
 URL = '/feed/{id}/'
-register(UserFactory, "user", name='name1', surname='surname1')
 
 
 @pytest.mark.django_db
-def test_notes(client, user):
-    n1 = NoteFactory()
-    n2 = NoteFactory()
-    UsersPostsFactory(users=user, posts=n1)
-    UsersPostsFactory(users=user, posts=n2)
-    url = URL.format(id=user.pk)
+def test_notes(client, user_with_notes):
+    url = URL.format(id=user_with_notes.pk)
 
     response = client.get(url)
 
@@ -25,12 +18,8 @@ def test_notes(client, user):
 
 
 @pytest.mark.django_db
-def test_achievement(client, user):
-    a1 = AchievementFactory(title='a1', body='b')
-    a2 = AchievementFactory(title='a2', body='b')
-    UsersPostsFactory(users=user, posts=a1)
-    UsersPostsFactory(users=user, posts=a2)
-    url = URL.format(id=user.pk)
+def test_achievement(client, user_with_achievements):
+    url = URL.format(id=user_with_achievements.pk)
 
     response = client.get(url)
 
@@ -40,14 +29,14 @@ def test_achievement(client, user):
 
 
 @pytest.mark.django_db
-def test_ad(client, user):
-    AdFactory(title='ad1', body='b', url='test.url')
+def test_ad(client, user, ad_1, ad_2):
     url = URL.format(id=user.pk)
 
     response = client.get(url)
 
     assert response.status_code == 200
     assert response.data[0]['post_type'] == Ad.POST_TYPE
+    assert len(response.data) == 1
 
 
 @pytest.mark.django_db
@@ -57,17 +46,16 @@ def test_ad(client, user):
         pytest.param('tn1', 'tn1'),
         pytest.param('a1', 'a1')
     ])
-def test_search(search_param, search_result, client, user):
-    n1 = NoteFactory(title='tn1', body='b')
-    a1 = AchievementFactory(title='a1', body='b')
-    UsersPostsFactory(users=user, posts=n1)
-    UsersPostsFactory(users=user, posts=a1)
+def test_search(search_param, search_result, client, user, create_note, create_users_posts):
+    create_users_posts(users=user, posts=create_note(title=search_param))
+    create_users_posts(users=user, posts=create_note(title="title"))
     url = URL.format(id=user.pk)
 
     response = client.get(url, data={'search': search_param})
 
     assert response.status_code == 200
     assert response.data[0]['title'] == search_result
+    assert len(response.data) == 1
 
 
 @pytest.mark.django_db
@@ -75,13 +63,8 @@ def test_search(search_param, search_result, client, user):
     pytest.param(FilterKeyWords.NOTE),
     pytest.param(FilterKeyWords.ACHIEVEMENT),
 ])
-def test_filter(filter_param, client, user):
-    for _ in range(3):
-        note = NoteFactory(title='tn', body='b')
-        achievement = AchievementFactory(title='a', body='b')
-        UsersPostsFactory(users=user, posts=note)
-        UsersPostsFactory(users=user, posts=achievement)
-    url = URL.format(id=user.pk)
+def test_filter(filter_param, client, user_with_notes_and_achievements):
+    url = URL.format(id=user_with_notes_and_achievements.pk)
 
     response = client.get(url, data={'filter': filter_param})
 
@@ -91,13 +74,8 @@ def test_filter(filter_param, client, user):
 
 
 @pytest.mark.django_db
-def test_filter_error(client, user):
-    for _ in range(3):
-        note = NoteFactory(title='tn', body='b')
-        achievement = AchievementFactory(title='a', body='b')
-        UsersPostsFactory(users=user, posts=note)
-        UsersPostsFactory(users=user, posts=achievement)
-    url = URL.format(id=user.pk)
+def test_filter_error(client, user_with_notes_and_achievements):
+    url = URL.format(id=user_with_notes_and_achievements.pk)
 
     response = client.get(url, data={'filter': "wrong_param"})
 
@@ -111,13 +89,8 @@ def test_filter_error(client, user):
     pytest.param(5, 0, 5),
     pytest.param(2, 1, 2),
 ])
-def test_pagination(limit, offset, data_len, client, user):
-    for _ in range(3):
-        note = NoteFactory(title='tn', body='b')
-        achievement = AchievementFactory(title='a', body='b')
-        UsersPostsFactory(users=user, posts=note)
-        UsersPostsFactory(users=user, posts=achievement)
-    url = URL.format(id=user.pk)
+def test_pagination(limit, offset, data_len, client, user_with_notes_and_achievements):
+    url = URL.format(id=user_with_notes_and_achievements.pk)
 
     response = client.get(url, data={'limit': limit, 'offset': offset})
 
